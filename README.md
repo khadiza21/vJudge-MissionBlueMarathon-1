503a40052548553e1c8a0c99ab3767fc715191db
khadiza21 <bibikhadiza474@gmail.com>
2022-04-11 03:20:52 +0600

    mission blue - marathon one in vJedge

---
+++ README.md
@@ -0,0 +1,4 @@
+# Mission Blue - First Marathon
+## Platform - vJudge
+### 20 Problems 
+#### Language : c++
--- test.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-int main()
-{
-	int a = 5, b = 2;
-	double marks = (double)a / (double)b;
-	printf("%lf", marks);
-}
