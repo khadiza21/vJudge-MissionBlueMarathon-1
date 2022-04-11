78a5267be4e4b11b202c44a22e673da01e4ced2f
khadiza21 <bibikhadiza474@gmail.com>
2022-04-12 03:12:22 +0600

    mission blue - marathon one in vJedge

--- R-howManyWays.cpp
+++ R-howManyWays.cpp
@@ -3,8 +3,31 @@ using namespace std;
 int main()
 {
 
-    int limitNum, sum, count = 0;
-    
+    int num1, num2, count ;
+    num1 = num2 = 1;
+    while (num1 != 0 || num2 != 0)
+    {
+        count=0;
+        cin >> num1 >> num2;
+        for (int i = 1; i <= num1; i++)
+        {
+            for (int j = i + 1; j <= num1; j++)
+            {
+                for (int k = j + 1; k <= num1; k++)
+                {
+                    if (j + i + k == num2 && i != j != k)
+                    {
+                        count++;
+                    }
+                }
+            }
+        }
+
+        if (num1 != 0 || num2 != 0)
+        {
+            cout << count << endl;
+        }
+    }
 
     return 0;
 }
