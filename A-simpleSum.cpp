635bc6bb917a6b7836ac45bcd570d5fa5a2be8f9
khadiza21 <bibikhadiza474@gmail.com>
2022-04-14 00:55:17 +0600

    firebase react email auth

--- A-simpleSum.cpp
+++ A-simpleSum.cpp
@@ -3,7 +3,7 @@ using namespace std;
 int main(){
     int a,b;
     cin>>a>>b;
-    if(0<=a && b<=10){
-        cout<<a+b<<endl;
-    }
+ 
+   cout <<a+b<<endl;
+  
 }
---
+++ B2-sumDiffer.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+
+    int a, b;
+    // float c, d;
+    int sum = a+b;
+    scanf("%d %d", &a, &b);
+    // scanf("%f %f", &c, &d);
+    // printf("%d %d", a + b, a - b);
+    // printf("\n%.1f %.1f", c + d, c - d);
+    printf("\n %d", sum);
+    return 0;
+}
