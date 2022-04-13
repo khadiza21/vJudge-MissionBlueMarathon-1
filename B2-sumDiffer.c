e104e2fe5b3f9c99e7b5a6fa23c57eed40fc53bb
khadiza21 <bibikhadiza474@gmail.com>
2022-04-14 04:45:50 +0600

    problem solved all

--- B2-sumDiffer.c
+++ B2-sumDiffer.c
@@ -4,12 +4,12 @@ int main()
 {
 
     int a, b;
-    // float c, d;
-    int sum = a+b;
+    float c, d;
+    int sum = a + b;
     scanf("%d %d", &a, &b);
-    // scanf("%f %f", &c, &d);
-    // printf("%d %d", a + b, a - b);
-    // printf("\n%.1f %.1f", c + d, c - d);
-    printf("\n %d", sum);
+    scanf("%f %f", &c, &d);
+    printf("%d %d", a + b, a - b);
+    printf("\n%.1f %.1f", c + d, c - d);
+
     return 0;
 }
